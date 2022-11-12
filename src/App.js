b1f5d68f5b07d833895605c0a3771ff0e16bcbc6
atulkandyong <atul@blocktech.dk>
2022-11-12 07:00:05 +0530

    Initial changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { FormBuilder, FormGenerator } from 'cb-react-forms';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Card from 'react-bootstrap/Card';
+import 'json-form-custom-element';
+
 
 function App() {
+  const [show, setShow] = useState(false);
+  let formData1 = [];
+  const [formJson, setformJson] = useState({
+    type: "object",
+    properties: {
+    }
+  });
+
+  const updateElements = () => {
+    formData1.forEach((e) => {
+      formJson.properties[e.id] = {
+        title: e.label.blocks[0].text,
+        type: "string"
+      }
+    });
+  };
+
+  const handleClose = () => setShow(false);
+
+  const items = [
+    {
+      key: "Header",
+      name: "Header Text",
+      icon: "fa fa-header"
+    },
+    {
+      key: "TextInput",
+      name: "TextInput",
+      icon: "fa fa-paragraph"
+    },
+
+  ];
+
+  const onSubmitFunc = (data) => {
+    formData1 = JSON.parse(data);
+    updateElements();
+    setShow(true);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FormBuilder items={items}
+        onSubmit={onSubmitFunc}
+      />
+
+      <Modal show={show} onHide={handleClose} >
+        <Modal.Header closeButton>
+          <Modal.Title>Form Preview</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Container>
+            <Row>
+              <Col md={12}>
+                <Card>
+                  <Card.Body>
+                    <json-form schema={JSON.stringify(formJson)} value='{}' config='{}'></json-form>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            <Row>
+              <Col md={12}>
+                <Card>
+                  <Card.Body>
+                    <code>
+                      {JSON.stringify(formJson)}
+                    </code>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+          </Container>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 }
 
